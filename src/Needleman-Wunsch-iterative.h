96f6435f438c42af15906d7fb1f15456eeb46255
Nabil Es-salihi <essalihinabil@gmail.com>
2022-11-06 12:31:09 +0100

    Deleting ierative files

--- src/Needleman-Wunsch-iterative.h
+++
@@ -1,8 +0,0 @@
-// #ifndef _FILENAME_H_
-// #define _FILENAME_H_
-
-// #include <stdlib.h>
-
-// long EditDistance_NW_iterative(char* A, size_t lengthA, char* B, size_t lengthB);
-
-// #endif /* _FILENAME_H_ */
